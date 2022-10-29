8fbe183f67196088da8cc0dddc5ec4bb06b081b8
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-10-29 19:51:59 -0300

    modif back

--- api/src/controllers/queryRecipe-controller.js
+++ api/src/controllers/queryRecipe-controller.js
@@ -5,16 +5,16 @@ const { getAllRecipes } = require('./data-controller');
 const getQueryRecipe = async (req, res) => {
   const { name } = req.query;
   const allRecipes = await getAllRecipes();
-
+  console.log(allRecipes);
   if (name) {
     const recipesName = allRecipes.filter((e) =>
-      e.name.toLowerCase().includes(name.toLowerCase())
+      e.title.toLowerCase().includes(name.toLowerCase())
     );
     recipesName.length
       ? res.status(200).send(recipesName)
       : res.status(404).send("No recipes found");
   } else {
-    res.status(200).send(allRecipes);
+    return res.status(200).send(allRecipes);
   }
 };
 
--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -8,65 +8,6 @@ const { validateRecipe, createRecipe } = require('../controllers/postRecipe-cont
 const { getQueryRecipe } = require('../controllers/queryRecipe-controller');
 
 
-//middleware
-
-/* GET recipes listing. */
-
-// const getApiData = async () => {
-//   const apiUrl = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=100&addRecipeInformation=true`);
-
-//   const apiInfo = await apiUrl.data.results.map(e => {
-//       return {
-//           id: e.id,
-//           title: e.title,
-//           dishTypes: e.dishTypes.map(e => e),
-//           summary: e.summary,
-//           healthScore: e.healthScore,
-//           instructions: e.analyzedInstructions.map(e => e.steps.map(e => e.step)).flat(),
-//           image: e.image,
-//           diets: e.diets.map(e => e),
-//       }
-//   });
-//   //console.log(apiInfo);
-//   return apiInfo; 
-// };
-
-// const getDbData = async () => {
-//   const dbInfo = await Recipe.findAll({
-//       include: {
-//           model: TypeDiet,
-//           attributes: ['name'],
-//           through: {
-//               attributes: []
-//           },
-//       }
-//   });
-
-//   return dbInfo;
-// }
-
-// const getAllRecipes = async() => {
-//   const apiInfo = await getApiData();
-//   const dbInfo = await getDbData();
-//   const allInfo = apiInfo.concat(dbInfo);
-//   return allInfo;
-// }
-
-// router.get("/", async (req, res) => {
-//   const { name } = req.query;
-//   const allRecipes = await getAllRecipes();
-
-//   if (name) {
-//     const recipesName = allRecipes.filter((e) =>
-//       e.name.toLowerCase().includes(name.toLowerCase())
-//     );
-//     recipesName.length
-//       ? res.status(200).send(recipesName)
-//       : res.status(404).send("No recipes found");
-//   } else {
-//     res.status(200).send(allRecipes);
-//   }
-// });
 router.get('/', getQueryRecipe);
 
 router.get("/:id", async (req, res) => {

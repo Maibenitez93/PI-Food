97a6435695fb7dafd54afdf36c869964e7a0997a
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-10-29 19:56:27 -0300

    modif back

--- api/src/controllers/queryRecipe-controller.js
+++ api/src/controllers/queryRecipe-controller.js
@@ -4,18 +4,23 @@ const { getAllRecipes } = require('./data-controller');
 
 const getQueryRecipe = async (req, res) => {
   const { name } = req.query;
-  const allRecipes = await getAllRecipes();
-  console.log(allRecipes);
-  if (name) {
-    const recipesName = allRecipes.filter((e) =>
-      e.title.toLowerCase().includes(name.toLowerCase())
-    );
-    recipesName.length
-      ? res.status(200).send(recipesName)
-      : res.status(404).send("No recipes found");
-  } else {
-    return res.status(200).send(allRecipes);
+  try {
+    const allRecipes = await getAllRecipes();
+
+    if (name) {
+      const recipesName = allRecipes.filter((e) =>
+        e.title.toLowerCase().includes(name.toLowerCase())
+      );
+      recipesName.length
+        ? res.status(200).send(recipesName)
+        : res.status(404).send("No recipes found");
+    } else {
+      return res.status(200).send(allRecipes);
+    }
+  } catch (error) {
+    return res.status(400).json({ err: err });
   }
+
 };
 
 module.exports = { getQueryRecipe };

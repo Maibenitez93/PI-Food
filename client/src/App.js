86e2a065115c94f38da9d8f2ce9ff8cfce6c1360
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-01 00:03:38 -0300

    modif App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import  Landing  from "./components/Landing/Landing";
-import './App.css';
 import Home from "./components/Home/Home";
+import './App.css';
 
 function App() {
   return (

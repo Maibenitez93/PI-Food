12aebe9e95c9ee1da08b45330bb4365819e9c9fb
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-01 00:06:42 -0300

    Create store

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,15 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducer';
 import thunk from 'redux-thunk';
 
-const composeEnhancers =
-   (typeof window !== 'undefined' &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-   compose;
-
-const store = createStore(
-   rootReducer,
-   composeEnhancers(applyMiddleware(thunk)),
-);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;

b97aef52d32b5377825b074dc7ede85b8b63c6cd
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-07 20:42:02 -0300

    Modif Route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import  Landing  from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
+import RecipeCreate from "./components/RecipeCreate/RecipeCreate";
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Route exact path='/' component={Landing}/>
       <Route exact path='/home' component={Home}/>
+      <Route exact path='/recipe' component={RecipeCreate}/>
     </div>
   );
 }

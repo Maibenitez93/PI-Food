36c1977bc6d188a6f69aed8659b1e1a26fd9b0da
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-09 00:39:25 -0300

    modif hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,22 +1,46 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postRecipe } from '../redux/actions';
+import { useHistory } from 'react-router-dom';
 
 export function useForm (initialForm, validateForm) {
   const [form, setForm] = useState(initialForm);
   const [errors, setErrors] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [response, setResponse] = useState(null);
 
-  const handleChange = (e) => {};
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  const handleBlur = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
-  const handleSubmit = (e) => {};
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validateForm(form));
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(postRecipe(form));
+
+    setErrors(validateForm(form));
+    
+    if (Object.keys(errors).length === 0) {
+      alert('Recipe created successfully');
+      setForm(initialForm);
+      history.push('/home');
+    } else {
+      alert('Recipe not created');
+    }
+  };
+    
     return {
         form,
         errors,
-        loading,
-        response,
         handleChange,
         handleBlur,
         handleSubmit

28ba4d3f556b29208ae8b6f725a29c90f22c5a73
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-09 00:38:18 -0300

    validate

--- client/src/components/RecipeCreate/RecipeForm.js
+++ client/src/components/RecipeCreate/RecipeForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useForm } from '../../hooks/useForm';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import { postRecipe, getTypesOfDiet } from '../../redux/actions';
+import { Link } from 'react-router-dom';
+import { getTypesOfDiet } from '../../redux/actions';
 
 const initialForm = {
   title: "",
@@ -17,21 +17,52 @@ const initialForm = {
 
 const validationsForm = (form) => {
     let errors = {};
-    
+
+    let regexString = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+      
     if (!form.title.trim()) {
         errors.title = "Title is required";  
+    } else if(!regexString.test(form.title.trim())) {
+        errors.title = "Title must be only letters";
+    } else if (form.title.length > 80 || form.title.length < 3) {
+        errors.title = "Title must be between 3 and 80 characters";
     }
-   
+
+    if (!form.summary.trim()) {
+      errors.summary = "Summary is required";  
+    } else if(!regexString.test(form.summary.trim())) {
+      errors.title = "Summary must be only letters";
+    } else if (form.summary.length > 255 || form.summary.length < 3) {
+      errors.summary = "Summary must be between 3 and 255 characters";
+    }
+
+    if (!form.healthScore) {
+      errors.healthScore = "HealthScore is required";  
+    } else if (!Number.isInteger(form.healthScore)) {
+      errors.healthScore = "HealthScore must be an integer";
+    } else if (form.healthScore < 1 || form.healthScore > 100) {
+      errors.healthScore = "HealthScore must be between 1 and 100";
+    }
+
+    if (!form.instructions.trim()) {
+      errors.instructions = "Instructions is required";  
+    } else if ( form.instructions.length < 3 || form.instructions.length > 1000) {
+      errors.instructions = "Instructions must be between 3 and 1000 characters";
+    }
+
+    if (form.image) {
+      if (!form.image.includes("http")) {
+        errors.image = "Image must be a valid URL";
+      } 
+    }
+
     return errors;
 }
-
 
 export default function RecipeForm() {
     const {
       form,
       errors,
-      loading,
-      response,
       handleChange,
       handleBlur,
       handleSubmit,
@@ -39,8 +70,7 @@ export default function RecipeForm() {
 
     const dispatch = useDispatch();
     const diets = useSelector((state) => state.diets);
-    const history = useHistory();
-
+    
     useEffect(() => {
         dispatch(getTypesOfDiet());
     }, [dispatch]);
@@ -55,7 +85,7 @@ export default function RecipeForm() {
             type="text"
             name="title"
             placeholder="Title"
-            onBlur={handleBlur}
+            onBlur={(e) => handleBlur(e)}
             value={form.title}
             onChange={(e) => handleChange(e)}
            
@@ -68,16 +98,17 @@ export default function RecipeForm() {
             type="text"
             name="summary"
             placeholder="Summary"
-            onBlur={handleBlur}
+            onBlur={(e) => handleBlur(e)}
             value={form.summary}
             onChange={(e) => handleChange(e)}
             required
           />
         </div>
-        <div class="multiselect">
-          <div class="selectBox">
+        {errors.summary && <p>{errors.summary}</p>}
+        <div className="multiselect">
+          <div className="selectBox">
             <span>Dish Types</span>
-            <div class="overSelect"></div>
+            <div className="overSelect"></div>
           </div>
           <div id="checkboxes">
             <label for="one">
@@ -140,22 +171,24 @@ export default function RecipeForm() {
             type="number"
             name="healthScore"
             value={form.healthScore}
-            onBlur={handleBlur}
+            onBlur={(e) => handleBlur(e)}
             onChange={(e) => handleChange(e)}
             required
           />
         </div>
+        {errors.healthScore && <p>{errors.healthScore}</p>}
         <div>
           <label>Instructions</label>
           <input
             type="text"
             name="instructions"
             value={form.instructions}
-            onBlur={handleBlur}
+            onBlur={(e) => handleBlur(e)}
             onChange={(e) => handleChange(e)}
             required
           />
         </div>
+        {errors.instructions && <p>{errors.instructions}</p>}
         <div>
           <span>Type of Diet</span>
           <br />
@@ -172,11 +205,12 @@ export default function RecipeForm() {
           <input
             type="text"
             name="image"
-            onBlur={handleBlur}
+            onBlur={(e) => handleBlur(e)}
             value={form.image}
             onChange={(e) => handleChange(e)}
           />
         </div>
+        {errors.image && <p>{errors.image}</p>}
         <button type="submit">Create Recipe</button>
       </form>
       <Link to="/home">

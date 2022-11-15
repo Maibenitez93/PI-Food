c5ed62a1ba7c1b71e7b5f5446ba9c98632255bc8
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-14 21:57:19 -0300

    Add getRecipeById action

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -4,9 +4,12 @@ import {
   GET_TYPES_OF_DIET,
   GET_RECIPES_BY_NAME,
   POST_RECIPES,
+  //DELETE_RECIPE,
   FILTER_BY_DIET,
   FILTER_CREATED,
   ORDER_BY_NAME,
+  ORDER_BY_SCORE,
+  GET_DETAIL
 } from "../actiones-types";
 
 export function getRecipes() {
@@ -51,7 +54,7 @@ export function getRecipeByName(name) {
 export function postRecipe(payload) {
     return async function (dispatch) {
         try {
-            return await axios.post('http://localhost:3001/recipe', payload)
+            return await axios.post('http://localhost:3001/recipes', payload)
             .then(res => {
                 dispatch({ type: POST_RECIPES, payload: res.data })
             });
@@ -79,5 +82,25 @@ export function orderByName(payload) {
     return {
         type: ORDER_BY_NAME,
         payload
+    };
+}
+
+export function orderByScore(payload) {
+    return {
+        type: ORDER_BY_SCORE,
+        payload
+    };
+}
+
+export function getDetail(id ) {
+    return async function (dispatch) {
+        try {
+            return await axios.get(`http://localhost:3001/recipes/${id}`)
+            .then(res => {
+                dispatch({ type: GET_DETAIL, payload: res.data })
+            });
+        } catch (error) {
+            console.log(error);
+        }
     };
 }

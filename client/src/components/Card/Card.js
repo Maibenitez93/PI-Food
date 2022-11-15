58a06a5ef9b3b81b08ca868787d67b43199f512c
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-14 21:52:10 -0300

    Add recipe card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 function Card({ name, diets, image }) {
   return (
-    <div>
+    <div className='container'>
+      <div className='card'>
         <h3>{name}</h3>
         <img src={image} alt={name} width='200px' height='250px' />
         <h5>{diets}</h5>
+      </div>
     </div>
   )
 };
-
 
 export default Card;

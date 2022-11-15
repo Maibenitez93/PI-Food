ec9b86c8cfe1d3703aeee3bf9a428594d944abef
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-14 21:51:20 -0300

    Add details route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import  Landing  from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
+import RecipeDetail from "./components/Detail/RecipeDetail";
 //import RecipeCreate from "./components/RecipeCreate/RecipeCreate";
 import RecipeForm from "./components/RecipeCreate/RecipeForm";
 import './App.css';
@@ -11,7 +12,8 @@ function App() {
     <div className="App">
       <Route exact path='/' component={Landing}/>
       <Route exact path='/home' component={Home}/>
-      <Route exact path='/recipe' component={RecipeForm}/>
+      <Route exact path='/recipes/:id' component={RecipeDetail}/>
+      <Route exact path='/recipe/create' component={RecipeForm}/>
     </div>
   );
 }

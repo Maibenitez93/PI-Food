a29299b83e8d545d6d3651a9cf108a6f5a07c3f0
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-11-14 21:47:24 -0300

    check rout id

--- api/src/controllers/idRecipe-controller.js
+++ api/src/controllers/idRecipe-controller.js
@@ -3,13 +3,16 @@ const { getAllRecipes } = require('./data-controller');
 const getRecipeById = async (req, res) => {
   const { id } = req.params;
   const allRecipes = await getAllRecipes();
-
-  if (id) {
-    const recipeId = allRecipes.find((e) => e.id == id);
-
-    recipeId
-      ? res.status(200).send(recipeId)
-      : res.status(404).send('Recipe not found');
+  try {
+    if (id) {
+      const recipeId = allRecipes.find((e) => e.id == id);
+  
+      recipeId
+        ? res.status(200).send(recipeId)
+        : res.status(404).send('Recipe not found');
+    }
+  } catch (err) {
+    return res.status(400).json({ err: err });
   }
 };
 
